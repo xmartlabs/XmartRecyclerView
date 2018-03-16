266e037f4aaa73df2ebadbbd00a75ae3a06d9ade
Matías Irland <matiasirland@gmail.com>
2018-03-16 11:02:41 -0300

    Add the loading state feature to the RecyclerViewEmptySupport

---
+++ library/src/main/java/com/xmartlabs/xmartrecyclerview/emptysupport/IsInLoadingStateProvider.java
@@ -0,0 +1,7 @@
+package com.xmartlabs.xmartrecyclerview.emptysupport;
+
+/** Used to prove the loading mode state. */
+public interface IsInLoadingStateProvider {
+  /** Returns if the list is in loading mode or not. */
+  boolean isInLoadingState();
+}
